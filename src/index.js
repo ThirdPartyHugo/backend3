3a8a27471ecb911b96ad2a4ea2fa7c481f787935
ThirdPartyHugo <118792142+ThirdPartyHugo@users.noreply.github.com>
2024-11-13 01:11:57 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
